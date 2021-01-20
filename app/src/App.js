5e84d0d2c805472f95e5359ba9de5573901f42d2
lljmb <brown.jessm@icloud.com>
2021-01-20 17:47:37 -0500

    completed projectgit add .git add .!

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Facts from './components/Facts';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Who's the King of Useless Information?</h1>
+      <h2>👉🏽👈🏽</h2>      
+      <Facts />
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/components/Facts.js
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getFact } from '../actions';
+
+const Facts = ({fact, isFetching, error, getFact }) => {
+
+    useEffect(() => {
+        getFact();
+      }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+    const handleClick = () => {
+        getFact();
+    }
+
+    if(error){
+        return <h3>Whoopsie daisy. Something broke: {error}</h3>;
+    }
+
+    if(isFetching){
+        return <h3>Get right with ya!</h3>
+    }
+
+    return(
+        <>
+        <h3>Ran(dumb): {fact}</h3>
+        <button onClick={handleClick}>Get another one!</button>
+        </>
+    )
+
+}
+
+const mapStateToProps = state => {
+    return {
+        fact: state.fact,
+        isFetching: state.isFetching,
+        error: state.error
+    };
+};
+
+export default connect(mapStateToProps, { getFact })(Facts)
